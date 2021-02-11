3c75b48918b0967930569ceea5b7424fd838bab8
Charlie Zheng <chzheng@protonmail.com>
2021-02-10 18:13:56 -0600

    Persistent database

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
+import argparse
 import io
 import json
 import os
-#import sqlite3
+import re
+import sqlite3
+
 from slack_bolt import App
 
-# TODO: Database
-#conn = sqlite3.connect('tasks.db')
-#conn.execute('CREATE TABLE IF NOT EXISTS tasks
-#    (name TEXT)')
-
 config = json.load(open('config.json'))
-tasks = []
+
+# Database
+conn = sqlite3.connect('tasks.db')
+with conn:
+    conn.execute('CREATE TABLE IF NOT EXISTS tasks (task TEXT, date INTEGER, person TEXT)')
+
+# Argument parsing
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(dest='subcommand')
+add_parser = subparsers.add_parser('add')
+add_parser.add_argument('task')
+remove_parser = subparsers.add_parser('remove')
+remove_parser.add_argument('task')
+show_parser = subparsers.add_parser('show')
+
+# TODO: Regex arguments
+#arg_pattern = re.compile('', re.IGNORECASE)
 
 app = App(
     token = config['token'],
     signing_secret = config['signingSecret'])
 
-@app.command('/add-task')
-def add_task(ack, say, command):
+@app.command('/task')
+def task(ack, say, command):
     ack()
-    tasks.append(command['text'])
-    # Show tasks
-    response = ''
-    for i in range(len(tasks)):
-        response = response + str(i + 1) + '. ' + tasks[i] + '\n'
-    say(response)
-    pass
-
-@app.command('/completed')
-def completed(ack, say, command):
-    ack()
-    tasks.pop(int(command['text']) - 1)
-    say('Task removed')
-    pass
-
-app.start(port = config['port'])
+    try:
+        # TODO: Complex splitting
+        args = parser.parse_args(command['text'].split(' '))
+    except:
+        say('Argument error')
+        return
+    conn = sqlite3.connect('tasks.db')
+    with conn:
+        if args.subcommand == 'add':
+            conn.execute('INSERT INTO tasks VALUES (?, NULL, NULL)',
+                (args.task,))
+        elif args.subcommand == 'remove':
+            conn.execute('DELETE FROM tasks WHERE task=?', (args.task,))
+        elif args.subcommand == 'show':
+            response = ''
+            i = 1
+            for row in conn.execute('SELECT * FROM tasks'):
+                response = response + str(i) + '. ' + row[0] + '\n'
+                i = i + 1
+            say(response)
