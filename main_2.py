dfefaf3dac7d7610db41aad053e53640bf6cc4f0
Molly Pribble <mollypribble1@mac.com>
2021-02-16 14:10:01 -0600

    Get data from db, trying to view home page

--- main_2.py
+++ main_2.py
@@ -30,9 +30,6 @@ app = App(
     token = config['token'],
     signing_secret = config['signingSecret'])
 
-if __name__ == "__main__":
-    app.start(port=int(os.environ.get("PORT", 3000)))
-
 @app.command('/task')
 def task(ack, say, command):
     ack()
@@ -57,85 +54,149 @@ def task(ack, say, command):
                 i = i + 1
             say(response)
 
-# home page header
-main_block = [
-    {
-    "type": "divider"
-    },
-    {
-    "type": "section",
-    "text": {
-        "type": "mrkdwn",
-        "text": "Welcome to Slacker, an interactive to-do list application. This is your home page, where you can view and edit your to-do list."
-            }
-        },
-    {
-    "type": "divider"
-    },
-    {
-    "type": "actions",
-    "elements": [
-        {
-            "type": "button",
-            "text": {
-                "type": "plain_text",
-                "text": "Add New Task",
-                "emoji": true
-                    },
-            "value": "click_me_123",
-            "action_id": "actionId-0"
-        }
-        ]
-    },
-    {
-    "type": "divider"
-    },
-    {
-    "type": "header",
-    "text": {
-        "type": "plain_text",
-        "text": "Team Tasks",
-        "emoji": true
-        }
-    },
-    {
-    "type": "divider"
-    }
-]
+# # home page header
+# main_block = [
+#     {
+#     "type": "divider"
+#     },
+#     {
+#     "type": "section",
+#     "text": {
+#         "type": "mrkdwn",
+#         "text": "Welcome to Slacker, an interactive to-do list application. This is your home page, where you can view and edit your to-do list."
+#             }
+#         },
+#     {
+#     "type": "divider"
+#     },
+#     {
+#     "type": "actions",
+#     "elements": [
+#         {
+#             "type": "button",
+#             "text": {
+#                 "type": "plain_text",
+#                 "text": "Add New Task",
+#                 "emoji": true
+#                     },
+#             "value": "click_me_123",
+#             "action_id": "actionId-0"
+#         }
+#         ]
+#     },
+#     {
+#     "type": "divider"
+#     },
+#     {
+#     "type": "header",
+#     "text": {
+#         "type": "plain_text",
+#         "text": "Team Tasks",
+#         "emoji": true
+#         }
+#     },
+#     {
+#     "type": "divider"
+#     }
+# ]
+#
+# # basic task block --> update with more features after it starts working
+# task_block = [
+#     {
+#     "type": "context",
+#     "elements": [
+#         {
+#         "type": "markdown",
+#         "text": "PLACEHOLDER" # make responsive  to input from task list
+#         }
+#     ]
+#     },
+#     {
+#     "type": "divider"
+#     }
+# ]
 
-# basic task block --> update with more features after it starts working
-task_block = [
-    {
-    "type": "context",
-    "elements": [
-        {
-        "type": "markdown"
-        "text": "PLACEHOLDER" # make responsive  to input from task list
-        }
-    ]
-    },
-    {
-    "type": "divider"
-    }
-]
+# returns list of data from table
+def get_tasks(data):
+    cur = data.cursor()
+    data.execute("SELECT * FROM tasks")
+
+    rows = cur.fetchall()
+    ret_me = []
+
+    for r in rows:
+        ret_me.append(r)
+
+    return ret_me
 
 # no clue if this works, but takes in list of task names and adds appropriate number of blocks in UI
 # need function that gets all task from db
 def build_home(lst, head, task):
-    building = [head]
+    block = [head]
+
     for t in lst:
-        building.append(task)
-    return building
+        block.append(task)
+
+    return block
 
 # listens to events and is called when task if open
 @app.event("app_home_opened")
 def update_home(client, event, logger):
     try:
         client.views_publish(
-        user_id = event["user"],
-        view = {
-            build_home([], main_block, task_block) # <-- unfinished
+        user_id= event["user"],
+        view= {
+            "type": "home",
+            "callback_id": "home_view",
+            "blocks": #build_home(get_tasks(conn), main_block, task_block)
+            [
+                {
+                "type": "divider"
+                },
+                {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Welcome to Slacker, an interactive to-do list application. This is your home page, where you can view and edit your to-do list."
+                        }
+                    },
+                {
+                "type": "divider"
+                },
+                {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Add New Task",
+                            "emoji": true
+                                },
+                        "value": "click_me_123",
+                        "action_id": "actionId-0"
+                    }
+                    ]
+                },
+                {
+                "type": "divider"
+                },
+                {
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Team Tasks",
+                    "emoji": true
+                    }
+                },
+                {
+                "type": "divider"
+                }
+            ]
         }
-        )
+    )
     except Exception as e:
         logger.error(f"Error publishing home tabe: {e}")
+
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
